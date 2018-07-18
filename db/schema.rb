aae481db7e5c1f4fa077cb212e70f515d52142e6
Matthew Tibby <matthewtibby@hotmail.com>
2018-07-18 10:01:12 +0000

    Edited plans table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_090454) do
+ActiveRecord::Schema.define(version: 2018_07_18_095757) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
