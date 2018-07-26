20d555023d917206dfee7c6b53652f01e3787473
Matthew Tibby <matthewtibby@hotmail.com>
2018-07-26 09:10:39 +0000

    updated user profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,25 +1,25 @@
 class ProfilesController < ApplicationController
-  # GET to /users/:user_id/profile/new
+   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile details form
     @profile = Profile.new
   end
   
- # POST to /users/:user_id/profile
-def create
-  # Ensure that we have the user who is filling out form
-  @user = User.find( params[:user_id] )
-  # Create profile linked to this specific user
-  @profile = @user.build_profile( profile_params )
-  if @profile.save
-    flash[:success] = "Profile updated!"
-    redirect_to user_path( params[:user_id] )
-  else
-    render action: :new
+# POST to /users/:user_id/profile
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :new
+    end
   end
-end
-private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :avatar, :phone_number, :contact_email, :description, :location)
-  end
+ private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
 end
